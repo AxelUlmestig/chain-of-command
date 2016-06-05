b961f4b2634e611573e0d6bbc257f23830ad0bd3
Axel Ulmestig <axel.ulmestig@gmail.com>
2016-06-05 13:24:27 +0200

    added inSpace function

--- script/vector_functions.js
+++ script/vector_functions.js
@@ -14,5 +14,7 @@ var addVectors = function(v1, v2) {
 
 //checks if v1 is in the rectangle created between v2 and v3
 var inSpace = function(v1, v2, v3) {
-	return false;
+	var inX = v1.x <= Math.max(v2.x, v3.x) && v1.x >= Math.min(v2.x, v3.x);
+	var inY = v1.y <= Math.max(v2.y, v3.y) && v1.y >= Math.min(v2.y, v3.y);
+	return inX && inY;
 }
