5878f00979f6e8d65b24d8fdfab8a05b7d4d15e6
Axel Ulmestig <axel.ulmestig@gmail.com>
2016-06-05 13:20:53 +0200

    added addVectors function

--- script/vector_functions.js
+++ script/vector_functions.js
@@ -6,7 +6,10 @@ var createVector = function(x, y) {
 };
 
 var addVectors = function(v1, v2) {
-	return v1;
+	return {
+		'x': v1.x + v2.x,
+		'y': v1.y + v2.y
+	}
 }
 
 //checks if v1 is in the rectangle created between v2 and v3
