e159db1ed114ab54b0b3ecc85f4e566ddf1b5e33
Axel Ulmestig <axel.ulmestig@gmail.com>
2016-06-05 18:57:56 +0200

    added bot_functions skeleton

---
+++ script/bot_functions.js
@@ -0,0 +1,45 @@
+directions = {
+	'NORTH': {
+		'name': 'N',
+		'value': createVector(0, 1),
+		'turn_right': 'EAST',
+		'turn_left': 'WEST'
+	},
+	'EAST': {
+		'name': 'E',
+		'value': createVector(1, 0),
+		'turn_right': 'SOUTH',
+		'turn_left': 'NORTH'
+	},
+	'SOUTH': {
+		'name': 'S',
+		'value': createVector(0, -1),
+		'turn_right': 'WEST',
+		'turn_left': 'EAST'
+	},
+	'WEST': {
+		'name': 'W',
+		'value' createVector(-1, 0),
+		'turn_right': 'NORTH',
+		'turn_left': 'SOUTH'
+	}
+}
+
+var initiateBot = function(x, y) {
+	return {
+		'position': createVector(x, y),
+		'direction': directions.NORTH
+	}
+}
+
+var moveBot = function(bot) {
+	return bot;
+}
+
+var turnBotRight = function(bot) {
+	return bot;
+}
+
+var turnBotLeft = function(bot) {
+	return bot;
+}
