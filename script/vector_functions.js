b0d38cbb1c52af8872d6c809670fc80577c16115
Axel Ulmestig <axel.ulmestig@gmail.com>
2016-06-05 13:16:40 +0200

    added vector_functions skeleton

---
+++ script/vector_functions.js
@@ -0,0 +1,15 @@
+var createVector = function(x, y) {
+	return {
+		'x': x, 
+		'y': y
+	};
+};
+
+var addVectors = function(v1, v2) {
+	return v1;
+}
+
+//checks if v1 is in the rectangle created between v2 and v3
+var inSpace = function(v1, v2, v3) {
+	return false;
+}
