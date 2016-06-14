324c60f1f60374a55cefe0d8bd9ee1c0e2362015
Axel Ulmestig <axel.ulmestig@gmail.com>
2016-06-14 21:12:08 +0200

    implemented minimum lang_controller

--- script/controllers/lang_controller.js
+++ script/controllers/lang_controller.js
@@ -1,3 +1,8 @@
 app.controller('lang_controller', function($scope, state) {
 	$scope.state = state;
+	$scope.languages = LANGUAGES;
+
+	$scope.selectedLangChanged = function() {
+		$scope.state.language = $scope.selectedLang;
+	}
 });
