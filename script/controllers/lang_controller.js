afeca457227e62f9ef5d739003726731b7be0857
Axel Ulmestig <axel.ulmestig@gmail.com>
2016-06-14 23:15:01 +0200

    updated the behaviour of the language selection in order to not get a stringified variable from angular

--- script/controllers/lang_controller.js
+++ script/controllers/lang_controller.js
@@ -1,8 +1,10 @@
 app.controller('lang_controller', function($scope, state) {
 	$scope.state = state;
 	$scope.languages = LANGUAGES;
-
-	$scope.selectedLangChanged = function() {
-		$scope.state.language = $scope.selectedLang;
+	
+	//I wanted to use '$scope.state.language = $scope.selectedLang;', but angular stringifies the 
+	//selectedLang variable making the functions in it disappear
+	$scope.selectedLangChanged = function(name) {
+		$scope.state.language = LANGUAGES[name];
 	}
 });
