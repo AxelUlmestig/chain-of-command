eda37182d39fb54ca354e71b6c96943e139d537d
Axel Ulmestig <axel.ulmestig@gmail.com>
2016-06-14 23:24:18 +0200

    hard coded space to a rectangle between (0, 0) and (10, 10)

--- script/controllers/space_controller.js
+++ script/controllers/space_controller.js
@@ -1,3 +1,12 @@
 app.controller('space_controller', function($scope, state) {
-	$scope.state = state;
+	var xStart = 5;
+	var yStart = 5;
+
+	var xCorner1 = 0;
+	var yCorner1 = 0;
+
+	var xCorner2 = 10;
+	var yCorner2 = 10;
+
+	state.space = createRectangularSpace(xStart, yStart, xCorner1, yCorner1, xCorner2, yCorner2);
 })
