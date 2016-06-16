d0a1fe48e8fe1e353a214f2529915727226f7b8a
Axel Ulmestig <axel.ulmestig@gmail.com>
2016-06-16 20:11:06 +0200

    added short description and list of dependencies

--- script/logic/space_functions.js
+++ script/logic/space_functions.js
@@ -1,18 +1,31 @@
 /*
- * The edges of the rectangle are assumed to be aligned with the 
- * coordinate system
+ * Contains functions related to creating and manipulating spaces.
+ * The sides of the rectangular spaces are aligned with the coordinate
+ * axis in order to save me a lot of cumbersome mathematics.
+ *
+ * Has dependencies to the following files/functions:
+ * 	
+ * 	script/logic/vector_functions.js
+ * 		createVector(x, y)
+ * 		inRectangle(v1, v2, v3)
+ * 		getVectorDistance(v1, v2)
+ *
+ * 	script/logic/bot_functions.js
+ *		initiateBot(x, y)
+ * 		
  */
+
 var createRectangleBoundary = function(v1, v2) {
 	return function(bot) {
 		var pos = bot.position;
-		return inRectangle(pos, v1, v2); //inRectangle defined in script/vector_functions.js
+		return inRectangle(pos, v1, v2);
 	}
 }
 
 var createCircleBoundary = function(center, radius) {
 	return function(bot) {
 		var pos = bot.position;
-		var distance = getVectorDistance(pos, center); //getVectorDistance defined in script/vector_functions.js
+		var distance = getVectorDistance(pos, center);
 		return distance <= radius
 	}
 }
@@ -41,6 +54,6 @@ var createRectangularSpace = function(xStart, yStart, xCorner1, yCorner1, xCorne
 
 var initiateBotInSpace = function(space) {
 	var startPosition = space.start_position;
-	var bot = initiateBot(startPosition.x, startPosition.y); //initiateBot defined in script/bot_functions.js
+	var bot = initiateBot(startPosition.x, startPosition.y);
 	return bot;
 }
