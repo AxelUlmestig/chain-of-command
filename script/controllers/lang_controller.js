5203b53f4fc39def0d1b157c3530abf8cacd5225
Axel Ulmestig <axel.ulmestig@gmail.com>
2016-06-16 21:26:41 +0200

    added short description and list of dependencies

--- script/controllers/lang_controller.js
+++ script/controllers/lang_controller.js
@@ -1,9 +1,19 @@
+/*
+ * Handles the select tag that allows the user to change the language.
+ *
+ * Has dependencies to the following files/functions:
+ * 	script/logic/interpreter.js
+ * 		LANGUAGES
+ *
+ */
 app.controller('lang_controller', function($scope, state) {
 	$scope.state = state;
 	$scope.languages = LANGUAGES;
 	
-	//I wanted to use '$scope.state.language = $scope.selectedLang;', but angular stringifies the 
-	//selectedLang variable making the functions in it disappear
+	/*
+	 * I wanted to use '$scope.state.language = $scope.selectedLang;', but angular stringifies the 
+	 * selectedLang variable making the functions in it disappear
+	 */
 	$scope.selectedLangChanged = function(name) {
 		$scope.state.language = LANGUAGES[name];
 	}
